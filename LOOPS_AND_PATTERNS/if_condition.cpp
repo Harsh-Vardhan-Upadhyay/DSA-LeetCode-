b6788c1de3fd7a6c5d208761ccc3281cd2869f19
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-01-27 03:02:40 +0530

    upadting loops codes

---
+++ LOOPS_AND_PATTERNS/if_condition.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int n;
+
+    cin>> n;
+    cin>> a;
+
+    if(a==n){
+        cout<<"yes they are the same"
+        <<endl;
+    }
+    else {
+        cout<<"Not the same"<<endl;
+    }
+    return 0;
+}
