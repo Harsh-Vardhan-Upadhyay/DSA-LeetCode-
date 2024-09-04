9547cf5ca141fb5b87f4a580e8fc41fd431bfbf7
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-09-05 03:40:01 +0530

    Time: 15 ms (52.85%), Space: 19.4 MB (83.11%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans =0;
+
+        int size = nums.size();
+        for( int i=0;i< size;i++){
+            ans=ans^nums[i];
+        }
+          return ans;
+    }
+  
+};
